714f613821376257f22994312b23468abbc7c7d2
minh.pham <minh.pham@stud.uni-goettingen.de>
2024-12-27 22:28:46 +0700

    Tested model & utils

--- utils/evaluator.py
+++ utils/evaluator.py
@@ -1,8 +1,12 @@
 import torch
 import numpy as np
 from tqdm.auto import tqdm
+from typing import Literal
 
-def evaluate_model(model, test_loader, device='cpu'):
+def evaluate_model(model, 
+                   test_loader: torch.utils.data.DataLoader,
+                   show_progress: bool=True,
+                   device: Literal['cpu', 'cuda']='cpu'):
     """
     Evaluate a PyTorch model.
 
@@ -12,14 +16,27 @@ def evaluate_model(model, test_loader, device='cpu'):
 
     :return: predictions and actual values
     """
+    print("Evaluation Configurations:")
+    print(f"Device: {device}")
+    print()
+
     model.eval()
     predictions = []
     actuals = []
-    with torch.no_grad():
-        for inputs, targets in tqdm(test_loader, desc='Evaluating', unit='batch'):
-            inputs = inputs.to(device).float()
-            outputs = model(inputs)
-            predictions.append(outputs.cpu().numpy())
-            actuals.append(targets.numpy())
+
+    if show_progress:
+        with torch.no_grad():
+            for inputs, targets in tqdm(test_loader, desc='Evaluating', unit='batch'):
+                inputs = inputs.to(device).float()
+                outputs = model(inputs)
+                predictions.append(outputs.cpu().numpy())
+                actuals.append(targets.numpy())
+    else:
+        with torch.no_grad():
+            for inputs, targets in test_loader:
+                inputs = inputs.to(device).float()
+                outputs = model(inputs)
+                predictions.append(outputs.cpu().numpy())
+                actuals.append(targets.numpy())
 
     return np.vstack(predictions), np.vstack(actuals)
